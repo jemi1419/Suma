3facb0eb9ea74a8bcd1f7e7d6bfeeb41ee6dc3b5
jemi1419 <camj080124hgtlrna2@cbtis217.edu.mx>
2024-10-16 08:39:34 -0600

    suma 4.0 vf

--- suma.cpp
+++ suma.cpp
@@ -5,8 +5,10 @@ int main() {
 	// este codigo hace una suma de a y b ahora con decimales
 	double a,b;
 	// leer a y b
+	cout << "leer";
 	cin >> a >> b;
-	// imprimir la suma redondeada a dos decimales 
+	// imprimir la suma redondeada a dos decimales
+	cout << "la suma es la suma de a y b\n"; 
 	cout << setprecision(2) << fixed<< a+b;
 	return 0;
 }
